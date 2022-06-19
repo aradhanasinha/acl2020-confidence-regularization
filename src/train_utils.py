83ae8568f7f02add49092f1990e5dcb2fc0ffecb
Anu Sinha <aradhanas@google.com>
2022-06-19 12:51:15 -0700

    add new shuffle that keeps mutual phrases together

--- src/train_utils.py
+++ src/train_utils.py
@@ -2,6 +2,7 @@
 Utils for train*.py.
 """
 import random
+import string
 from typing import Dict, Iterable, List
 from functools import total_ordering
 from collections import namedtuple
@@ -119,7 +120,9 @@ class ExampleConverter(Processor):
         input_ids=input_ids, segment_ids=segment_ids, label_id=label_id)
 
   @staticmethod
-  def _shuffle_sentence_like_word_ordering_paper(sentence, random_seed):
+  def _shuffle_sentence_like_word_ordering_paper(text_pair_example,
+                                                 random_seed):
+    sentence = text_pair_example.hypothesis
     if len(nltk.sent_tokenize(sentence)) > 1:
       return None
     tokens = nltk.word_tokenize(sentence)
@@ -129,35 +132,77 @@ class ExampleConverter(Processor):
     return " ".join(tokens)
 
   @staticmethod
-  def _dropout_tokens(sentence, random_seed, dropout_prob=0.10):
+  def _shuffle_hyp_but_keep_mutual_phrases(text_pair_example, random_seed):
+    new_hypothesis, _ = shuffle_but_keep_mutual_phrases_together(
+        text_pair_example.hypothesis, text_pair_example.premise, random_seed)
+    return new_hypothesis
+
+  @staticmethod
+  def _shuffle_premise_but_keep_mutual_phrases(text_pair_example, random_seed):
+    _, new_premise = shuffle_but_keep_mutual_phrases_together(
+        text_pair_example.hypothesis, text_pair_example.premise, random_seed)
+    return new_premise
+
+  @staticmethod
+  def _identity(text_pair_example, random_seed):
+    "Rely on the bert model's dropout during training."
+    return text_pair_example.hypothesis
+
+  @staticmethod
+  def _dropout_tokens(text_pair_example, random_seed, dropout_prob=0.10):
+    sentence = text_pair_example.hypothesis
     tokens = nltk.word_tokenize(sentence)
     num_tokens_to_keep = math.floor((1 - dropout_prob) * len(tokens))
     indicies_to_keep = random.Random(random_seed).sample(
         range(len(tokens)), num_tokens_to_keep)
-    tokens_to_keep = tokens #[tokens[i] for i in sorted(indicies_to_keep)]
+    tokens_to_keep = [tokens[i] for i in sorted(indicies_to_keep)]
     return " ".join(tokens_to_keep)
+
+  def get_aug_input_features_list(self,
+                                  text_pair_example,
+                                  aug_fn,
+                                  num_to_generate=None):
+    if num_to_generate is None:
+      num_to_generate = self.num_of_aug_generated
+    augmented_input_feature_list = []
+    for i in range(num_to_generate):
+      text_pair_example = aug_fn(text_pair_example, i)
+      input_feature = self._get_input_feature(text_pair_example.premise,
+                                              text_pair_example.hypothesis,
+                                              text_pair_example.label)
+      augmented_input_feature_list.append(input_feature)
+    return augmented_input_feature_list
 
   def create_input_features(self, text_pair_example):
     original_input_feature = self._get_input_feature(
         text_pair_example.premise, text_pair_example.hypothesis,
         text_pair_example.label)
 
-    def get_aug_input_features_list(hypothesis_aug_fn, num_to_generate=None):
-      if num_to_generate is None:
-          num_to_generate = self.num_of_aug_generated
-      augmented_input_feature_list = []
-      for i in range(num_to_generate):
+    def get_aughyp_input_features_list(hypothesis_aug_fn, num_to_generate=None):
+      def aug_fn(text_pair_example, i):
         aug_hypothesis = hypothesis_aug_fn(text_pair_example.hypothesis, i)
-        input_feature = self._get_input_feature(text_pair_example.premise,
-                                                aug_hypothesis,
-                                                text_pair_example.label)
-        augmented_input_feature_list.append(input_feature)
-      return augmented_input_feature_list
+        TextPairExample(text_pair_example.id, text_pair_example.premise,
+                        aug_hypothesis, text_pair_example.label)
+      return self.get_aug_input_features_list(text_pair_example, aug_fn,
+                                              num_to_generate)
 
-    shuffled_input_feature_list = get_aug_input_features_list(
-        ExampleConverter._shuffle_sentence_like_word_ordering_paper)
-    token_dropout_input_feature_list = get_aug_input_features_list(
-        ExampleConverter._dropout_tokens, 1)
+    def get_augprem_input_features_list(premise_aug_fn, num_to_generate=None):
+      def aug_fn(text_pair_example, i):
+        aug_premise = premise_aug_fn(text_pair_example.premise, i)
+        TextPairExample(text_pair_example.id, aug_premise,
+                        text_pair_example.hypothesis, text_pair_example.label)
+      return self.get_aug_input_features_list(text_pair_example, aug_fn,
+                                              num_to_generate)
+
+    half = self.num_of_aug_generated // 2
+    other_half = self.num_of_aug_generated - half
+    shuffled_input_feature_list = get_aughyp_input_features_list(
+        ExampleConverter._shuffle_hyp_but_keep_mutual_phrases,
+        half) + get_augprem_input_features_list(
+            ExampleConverter._shuffle_prem_but_keep_mutual_phrases, other_half)
+
+    token_dropout_input_feature_list = get_aughyp_input_features_list(
+        ExampleConverter._identity, 1)
 
     return InputFeatures(
         example_id=text_pair_example.id,
@@ -332,4 +377,127 @@ def acc_and_f1(preds, labels):
   f1_non = f1_score(y_true=labels, y_pred=preds, pos_label=0)
   return {"acc": acc, "f1": f1, "f1_non": f1_non}
 
+# Other augmentation functions.
+def word_tokenize_with_index(text):
+  tokens=nltk.word_tokenize(text)
+  offset = 0
+  result = []
+  for token in tokens:
+    offset = text.find(token, offset)
+    result.append((token.lower(), offset, offset+len(token)))
+    offset += len(token)
+  if result[-1][0] in string.punctuation:
+    end = result.pop()
+    return result, end
+  return result, None
+
+def shuffle_but_keep_mutual_phrases_together(s1, s2, seed=0):
+  """
+  Given two strings {s1} and {s2}, it returns a shuffling of their tokens:
+  - tokens are determined by nltk.word_tokenize
+  - If there is a common phrase of multiple tokens across the two
+    sentences, that phrase is kept together during the shuffle. What
+    constitutes a mutual phrase is case insenstive. The mutual phrases are
+    shuffled 50 times or until the phrases are not in their initial ordering.
+    This is necessary, since sometimes a sentence only has two mutual phrases
+    and we have to break intelligibility. 
+    This sometimes means that the shuffled sentence is larger than the original 
+    if the same token is part of two mutual phrases:
+       ex: The scientist ate the cake
+           The scientist ate the onion or ate the cake.
+           Here the mutual phrases are:
+              "The scientist ate the", and "the cake"
+           The current behavior includes both phrases, even though this does
+           increase the overall length of the sentence.
+  - If either sentence ends in a token present in string.punctuation, 
+    that token continues to be at the end of that sentence.
+  """
+  s1_tokens, s1_end = word_tokenize_with_index(s1)
+  s2_tokens, s2_end = word_tokenize_with_index(s2)
+
+  # Get substrings in common using token-wise index.
+  subtring_end_index_inclusive_to_len = {}
+  for i, (s1t, _, _) in enumerate(s1_tokens):
+    for j, (s2t, _, _) in enumerate(s2_tokens):
+      if s1t == s2t:
+        prev = subtring_end_index_inclusive_to_len.get((i-1, j-1), 0)
+        subtring_end_index_inclusive_to_len[(i, j)] = prev + 1
+        if prev != 0:
+          del subtring_end_index_inclusive_to_len[(i - 1, j - 1)]  # Save space.
+
+  def is_i_subset_of_j(i_start, i_length, j_start, j_length):
+    if j_start > i_start:  # second item starts strictly after first item.
+      return False
+    i_end = i_start + i_length
+    j_end = j_start + j_length
+    return j_end >= i_end  # second item ends strictly at or after first item.
+
+  def filter_subset_substrings(start_index_and_len):
+    # Sort length increasing, and then start_index increasing.
+    start_index_and_len.sort(key=lambda x: (x[1], x[0]))
+    min_start_index_inclusive_and_len = []
+    for i, (i_start, i_length) in enumerate(start_index_and_len):
+      next_items = start_index_and_len[i + 1:]
+      is_i_subset = any([
+          is_i_subset_of_j(i_start, i_length, j_start, j_length)
+          for (j_start, j_length) in next_items
+      ])
+      if not is_i_subset:
+        min_start_index_inclusive_and_len.append((i_start, i_length))
+    return min_start_index_inclusive_and_len
+
+  s1_start_index_and_len = filter_subset_substrings([
+      (e1 - length + 1, length)
+      for ((e1, _), length) in subtring_end_index_inclusive_to_len.items()
+  ])
+  s2_start_index_and_len = filter_subset_substrings([
+      (e2 - length + 1, length)
+      for ((_, e2), length) in subtring_end_index_inclusive_to_len.items()
+  ])
+
+  def get_substrings(text, tokens, start_index_and_len):
+    get_substring = lambda token_index_range: text[tokens[token_index_range[0]][
+        1]:tokens[token_index_range[1]][2]]
+    i = 0
+    substrings = []
+    is_phrase = []
+    for s, length in sorted(start_index_and_len):
+      for j in range(i, s):
+        substrings.append(get_substring((j, j)))
+        is_phrase.append(False)
+      i = s + length
+      substrings.append(get_substring((s, i - 1)))
+      is_phrase.append(length > 1)
+    for j in range(i, len(tokens)):
+      substrings.append(get_substring((j, j)))
+      is_phrase.append(False)
+    return substrings, is_phrase
+
+  s1_substrings, s1_is_phrase = list(
+      get_substrings(s1, s1_tokens, s1_start_index_and_len))
+  s2_substrings, s2_is_phrase = list(
+      get_substrings(s2, s2_tokens, s2_start_index_and_len))
+
+  is_sorted = lambda l: (all(l[i] <= l[i + 1] for i in range(len(l) - 1)))
+
+  def shuffle_and_join(text, substrings, substring_is_phrase, end=None):
+    phrase_indicies = set(i for i, b in enumerate(substring_is_phrase) if b)
+    substring_and_indices = list(enumerate(substrings))
+
+    for salt in range(50):  # Try 50 times to make sure phrases are shuffled.
+      random.Random(seed+salt).shuffle(substring_and_indices)
+      phrase_shuffled_indicies = [
+          x[0] for x in substring_and_indices if x[0] in phrase_indicies
+      ]
+      if not is_sorted(phrase_shuffled_indicies):
+        break
+    substrings = [x[1] for x in substring_and_indices]
+    new_text = " ".join(substrings)
+    if end is not None:
+      new_text += text[end[1]:end[2]]
+    return new_text
+  new_s1 = shuffle_and_join(s1, s1_substrings, s1_is_phrase, s1_end)
+  new_s2 = shuffle_and_join(s2, s2_substrings, s2_is_phrase, s2_end)
+
+  return new_s1, new_s2
 
